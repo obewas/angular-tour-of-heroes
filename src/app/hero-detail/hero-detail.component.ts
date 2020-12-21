e2f87c3e857348763cbdfd29f71d9d06909c6898
obewas <obewas1202@gmail.com>
2020-12-21 14:38:21 +0300

    added message service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+import { from } from 'rxjs';
+import { Hero } from '../hero'
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
